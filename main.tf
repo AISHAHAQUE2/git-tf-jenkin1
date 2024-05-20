450d904bf3ff43f5fee7bb860b54de2dcb89986d
AISHAHAQUE2 <92319665+AISHAHAQUE2@users.noreply.github.com>
2024-05-20 22:09:14 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,21 @@
+provider "google" {
+  project = "omega-jet-420717"
+  region  = "us-central1"
+}
+
+resource "google_compute_instance" "example" {
+  name         = "example-instance"
+  machine_type = "n1-standard-1"
+  zone         = "us-central1-a"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-9"
+    }
+  }
+
+  network_interface {
+    network = "default"
+    access_config {}
+  }
+}
