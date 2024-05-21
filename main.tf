e61f95a04a64a134e6218b3d64238aa9bf532c80
AISHAHAQUE2 <92319665+AISHAHAQUE2@users.noreply.github.com>
2024-05-21 11:30:39 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,21 @@
-resource "google_storage_bucket" "my-bucket" {
-  name                     = "tt-githubdemo-bucket-001"
-  project                  = "omega-jet-420717"
-  location                 = "US"
-  force_destroy            = true
-  public_access_prevention = "enforced"
+provider "google" {
+  project = "omega-jet-420717"
+  region  = ""us-west4""
+}
+
+resource "google_compute_instance" "example" {
+  name         = "example-instance"
+  machine_type = "n1-standard-1"
+  zone         = "us-west4-b"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-9"
+    }
+  }
+
+  network_interface {
+    network = "default"
+    access_config {}
+  }
 }
